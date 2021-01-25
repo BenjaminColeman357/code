67691b1a89a41e1c0f1e40d43dddffbb045b7923
BenjaminColeman357 <73182429+BenjaminColeman357@users.noreply.github.com>
2021-01-25 13:22:59 +0300

    Create ya.js

---
+++ ya.js
@@ -0,0 +1,17 @@
+var strGET = window.location.search.replace( '?', ''); 
+
+var params = window
+    .location
+    .search
+    .replace('?','')
+    .split('&')
+    .reduce(
+        function(p,e){
+            var a = e.split('=');
+            p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+            return p;
+        },
+        {}
+    );
+
+window.location.href="http://ya.ru";
